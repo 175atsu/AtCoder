38dbfb0905a7cbd1db7f9cf982e5a2ce6d1b6338
Atsu <astu.i.0601@gmail.com>
2020-04-17 23:22:21 +0900

    ABC1

---
+++ app/src/main/java/com/example/atcoder/ABC1.kt
@@ -0,0 +1,18 @@
+package com.example.atcoder
+
+//A - 積雪深差
+//fun main(args: Array<String>) {
+//    val a = readLine()!!.toInt()
+//    val b = readLine()!!.toInt()
+//    println(a-b)
+//}
+
+//B - 視程の通報
+fun main(args: Array<String>) {
+    var m = readLine()!!.toInt()/1000.toDouble()
+    if (m < 0.1) {
+        print("00")
+    } else if (m <= 0.5) {
+
+    }
+}
